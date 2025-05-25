e4658108b2208e03f5924fcb165fe43b9709f143
hiproz <hiproz@gmail.com>
2025-05-25 19:11:53 +0000

    fix ssl on warning and certbot non-interactive issue

--- wt.sh
+++ wt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-##  命令格式： wt.sh action domain  webroot  nginx-config-path
+##  命令格式： wt.sh action domain  webroot  nginx-config-path 1@2.com
 #eg: 
-#./wt.sh add blog.goodmemory.cc /data/wwwroot/blog_gm /usr/local/nginx/conf/vhost
+#./wt.sh add blog.goodmemory.cc /data/wwwroot/blog_gm /usr/local/nginx/conf/vhost 1@2.com
 #./wt.sh remove blog.goodmemory.cc
 
 # action: add remove 
@@ -11,6 +11,7 @@ action=$1
 domain=$2
 webroot=$3
 nginx_conf_path=$4
+email=$5
 
 if [ ! -d "~/.wt" ]; then
     mkdir -p ~/.wt
@@ -18,7 +19,7 @@ fi
 
 if [ "$action" = "add" ]; then
     echo "wt.sh [action] [domain] [webroot] [nginx-config-path]"
-    if [  $# -lt 4 ]; then
+    if [  $# -lt 5 ]; then
         echo "invalid params"
         exit 0
     fi
@@ -45,13 +46,16 @@ if [ "$action" = "add" ]; then
 
     ## 开始生成https证书
     # 执行certbot命令
-    certbot certonly --webroot -w $webroot -d $domain |grep .pem > ~/.wt/$domain
+    certbot certonly --non-interactive  --agree-tos --email $email --webroot -w $webroot -d $domain |grep .pem > ~/.wt/$domain
 
     ## 分别获取fullchain.pem 和 privkey.pem
     full_pem_path=$(cat ~/.wt/$domain|head -n 1)
     private_pem_path=$(sed -n '2p' ~/.wt/$domain)
     echo "letsencrypt pem path:$full_pem_path, $private_pem_path"
     echo "certbot success!"
+    
+    # todo 这里有bug，在替换的路径前面有额外多余的内容，待修复  20250526
+    #
     ## 到这里我们已经成功获取letsencrypt的https证书了，接下来就是生成最终的nginx conf 文件
     sed "s|BBB|$domain|g; s|AAA|$webroot|g; s|CCC|$full_pem_path|g; s|DDD|$private_pem_path|g" default-https.conf > $domain.conf
 
@@ -61,7 +65,12 @@ if [ "$action" = "add" ]; then
 elif [ "$action" = "remove"  ]; then
     pem_path=$(cat ~/.wt/$domain|head -n 1)
     echo "letsencrypt pem path :$pem_path"
-    yes Y | certbot revoke --force-interactive --cert-path $pem_path
+    yes Y | certbot revoke --non-interactive  --agree-tos --email $email --force-interactive --cert-path $pem_path
     rm -f ~/.wt/$domain
     nginx -s reload 
+else
+    echo "please input invalid params!"
+    echo "eg:"
+    echo "./wt.sh add blog.goodmemory.cc /data/wwwroot/blog_gm /usr/local/nginx/conf/vhost 1@2.com"
+    echo "./wt.sh remove blog.goodmemory.cc"
 fi
